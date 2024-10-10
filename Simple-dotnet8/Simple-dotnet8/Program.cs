e4979f7d1044f01049f8fb0f29ec32bdaff7fe90
Ivaylo Dimitrov <idi@milestone.dk>
2024-10-10 13:12:08 +0300

    add console app

---
+++ Simple-dotnet8/Simple-dotnet8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simple_dotnet8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
